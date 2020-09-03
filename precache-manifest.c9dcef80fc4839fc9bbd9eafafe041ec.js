8868bc9116c5d105c67c0d11a7d38c761ea201b1
Ahmed Raza <62701326+ahmedraza17260@users.noreply.github.com>
2020-09-04 00:46:59 +0500

    build folder

---
+++ precache-manifest.c9dcef80fc4839fc9bbd9eafafe041ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f00b4c947eb56166af453eeb063489e",
+    "url": "/ecommerce-shopping-basket/index.html"
+  },
+  {
+    "revision": "5c42815555cac8703054",
+    "url": "/ecommerce-shopping-basket/static/css/main.bcfbf20a.chunk.css"
+  },
+  {
+    "revision": "328eea669f885a765811",
+    "url": "/ecommerce-shopping-basket/static/js/2.5cfce51d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ecommerce-shopping-basket/static/js/2.5cfce51d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c42815555cac8703054",
+    "url": "/ecommerce-shopping-basket/static/js/main.097ce901.chunk.js"
+  },
+  {
+    "revision": "2fe2c6b1c6dc2dee894c",
+    "url": "/ecommerce-shopping-basket/static/js/runtime-main.c6fd1dc3.js"
+  }
+]);
